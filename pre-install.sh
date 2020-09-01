460f0d8525debc6053e95029f55cb8c2d81efc69
KMFtcy <kmftangchaoyang@gmail.com>
2020-09-01 09:34:27 +0800

    add pre-install script

---
+++ pre-install.sh
@@ -0,0 +1,4 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+&& git clone https://github.com/jacoborus/tender.vim ~/.vim/pack/vendor/start/tendor
