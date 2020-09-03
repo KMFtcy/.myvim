9cfe29d5dbd35493149b34d1f21a6ce3fdfdedbe
KMFtcy <kmftangchaoyang@gmail.com>
2020-09-03 08:58:00 +0800

    feat:now script will auto install plugin

--- pre-install.sh
+++ pre-install.sh
@@ -21,4 +21,4 @@ git clone https://github.com/jacoborus/tender.vim ~/.vim/pack/vendor/start/tendo
 curl https://nodejs.org/dist/v12.18.3/node-v12.18.3-linux-x64.tar.xz -o ~/node.tar.xz && xz -d ~/node.tar.xz && tar -xvf  ~/node.tar -C ~/ \
 	&& ln -s ~/node-v12.18.3-linux-x64/bin/node /usr/local/bin && ln -s ~/node-v12.18.3-linux-x64/bin/npm /usr/local/bin && ln -s ~/node-v12.18.3-linux-x64/bin/npx /usr/local/bin
 
-
+vim -c PlugInstall -c q -c q
