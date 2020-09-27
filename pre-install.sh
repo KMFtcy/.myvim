08445799e827d94d110842aac1059e9be38a91c2
root <root@DESKTOP-ONU8CEI.localdomain>
2020-09-27 11:37:38 +0800

    now don't need to input anything

--- pre-install.sh
+++ pre-install.sh
@@ -6,7 +6,7 @@ apt-get install gcc git ncurses-dev make python2-dev python3-dev -y
 # abandon silversearch, choose ripgre
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
-sudo add-apt-repository ppa:jonathonf/vim
+echo | sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update -y
 sudo apt-get install vim -y
 if [ -f "~/.vimrc" ]; then
@@ -18,8 +18,8 @@ ln -s $basepath/vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# curl https://nodejs.org/dist/v12.18.3/node-v12.18.3-linux-x64.tar.xz -o ~/node.tar.xz && xz -d ~/node.tar.xz && tar -xvf  ~/node.tar -C ~/ \
-# 	&& ln -s ~/node-v12.18.3-linux-x64/bin/node /usr/local/bin && ln -s ~/node-v12.18.3-linux-x64/bin/npm /usr/local/bin && ln -s ~/node-v12.18.3-linux-x64/bin/npx /usr/local/bin
-curl -sL install-node.now.sh/lts | bash
+curl -sL install-node.now.sh/lts > install_nodejs.sh
+yes | bash install_nodejs.sh
+rm install_nodejs.sh
 
 vim -c PlugInstall -c q -c q
