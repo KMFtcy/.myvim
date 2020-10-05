4371db4a11ef98178af87889d472ccd84c486e7b
root <root@DESKTOP-G61KQFL.localdomain>
2020-10-05 18:24:13 +0800

    update comments

--- pre-install.sh
+++ pre-install.sh
@@ -1,25 +1,29 @@
-apt-get update -y
+# env paramater
 basepath=`dirname $0`
+# move into work directory
 cd $HOME
+# download neccessary package and software
+apt-get update -y
+# basic dependency software
 apt-get install gcc git ncurses-dev make python2-dev python3-dev -y
-# apt-get install silversearcher-ag 
-# abandon silversearch, choose ripgre
+# ripgrep, supprt search feature
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
+# install the latest vim 
 echo | sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update -y
 sudo apt-get install vim -y
+# replace old vimrc
 if [ -f "~/.vimrc" ]; then
 	mv  ~/.vimrc ~/.vimrc.previous
 fi
-# install vimrc
 ln -s $basepath/vimrc ~/.vimrc
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+# intall nodejs, coc-nvim dependency
 curl -sL install-node.now.sh/lts > install_nodejs.sh
 yes | bash install_nodejs.sh
 rm install_nodejs.sh
-
+# install vim plugins
 vim -c PlugInstall -c q -c q
